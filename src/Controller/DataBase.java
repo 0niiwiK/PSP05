d80a7b9a85d88968c26651b29bcc5cc5c05725f8
Cole <israsanfuen@gmail.com>
2023-11-30 20:10:41 +0100

    Init 2

--- src/Controller/DataBase.java
+++ src/Controller/DataBase.java
@@ -1,36 +1,48 @@
 package Controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DataBase {
     String url = "jdbc:mysql://localhost:3307";
     String user = "root";
     String password = "mi-contrasena";
-    public DataBase() {
+    Statement stmt;
+    Connection conn;
+    ResultSet rs;
+    public DataBase() throws SQLException {
+        createConnection();
+        createStatement();
     }
-    public Connection connection() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connection to SQLite has been established.");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        return conn;
+    public void createConnection() throws SQLException {
+        conn = null;
+        conn = DriverManager.getConnection(url, user, password);
     }
 
-    public Statement createStatement(Connection conn) throws SQLException {
-        return conn.createStatement();
+    public void createStatement() throws SQLException {
+        stmt = conn.createStatement();
+    }
+
+    public Statement obtenerStatement() throws SQLException {
+        return stmt;
+    }
+
+    public ResultSet obtenerResultSet() throws SQLException {
+        return rs;
+    }
+
+    public void inicializar() throws SQLException {
+        stmt.executeUpdate("DROP SCHEMA IF EXISTS Tienda");
+        stmt.executeUpdate("CREATE SCHEMA IF NOT EXISTS Tienda");
+        stmt.executeUpdate("USE Tienda");
+        stmt.executeUpdate("DROP TABLE IF EXISTS Ejemplo");
+        stmt.executeUpdate("CREATE TABLE Ejemplo (id INT, nombre VARCHAR(255))");
+    }
+
+    public void cerrar() {
+        try {
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,17 @@ import java.sql.*;
 import Controller.DataBase;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        DataBase db = new DataBase();
+    public static void main(String[] args){
         try {
-            Statement stmt = db.createStatement(db.connection());
-            stmt.execute("SELECT * FROM 'users'");
+            DataBase db = new DataBase();
+            db.inicializar();
+            Statement stmt = db.obtenerStatement();
+            stmt.execute("INSERT INTO Ejemplo VALUES (1, 'Ejemplo')");
+            ResultSet rs = null;
+            rs = stmt.executeQuery("SELECT * FROM Ejemplo");
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + " " + rs.getString("nombre"));
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
