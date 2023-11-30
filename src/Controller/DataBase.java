2ab314762e157be8f5076e11f122221dc0d5afa2
Cole <israsanfuen@gmail.com>
2023-11-30 20:46:50 +0100

    Init 2

--- src/Controller/DataBase.java
+++ src/Controller/DataBase.java
@@ -26,16 +26,18 @@ public class DataBase {
         return stmt;
     }
 
-    public ResultSet obtenerResultSet() throws SQLException {
-        return rs;
-    }
-
     public void inicializar() throws SQLException {
         stmt.executeUpdate("DROP SCHEMA IF EXISTS Tienda");
         stmt.executeUpdate("CREATE SCHEMA IF NOT EXISTS Tienda");
         stmt.executeUpdate("USE Tienda");
-        stmt.executeUpdate("DROP TABLE IF EXISTS Ejemplo");
-        stmt.executeUpdate("CREATE TABLE Ejemplo (id INT, nombre VARCHAR(255))");
+        stmt.execute("DROP TABLE IF EXISTS Cliente;");
+        stmt.execute("CREATE TABLE IF NOT EXISTS Cliente (id INT NOT NULL AUTO_INCREMENT, user VARCHAR(20), password VARCHAR(15), PRIMARY KEY (id));");
+        stmt.execute("DROP TABLE IF EXISTS Producto;");
+        stmt.execute("CREATE TABLE IF NOT EXISTS Producto (id INT NOT NULL AUTO_INCREMENT, nombre VARCHAR(20), precio FLOAT(10,2), fecha_compra DATE, cliCliente INT, PRIMARY KEY (id), FOREIGN KEY (cliCliente) REFERENCES Cliente(id));");
+        stmt.execute("INSERT INTO Cliente (user, password) VALUES ('Usuario', 'Contrasenia');");
+        stmt.execute("INSERT INTO Producto (nombre, precio, fecha_compra, cliCliente) VALUES ('Manzana', 1.00, '2020-01-01', 1);");
+        stmt.execute("INSERT INTO Producto (nombre,precio, fecha_compra, cliCliente) VALUES ('Pera', 2.00, '2020-01-01', 1);");
+
     }
 
     public void cerrar() {
