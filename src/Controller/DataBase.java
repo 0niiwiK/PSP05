b3ddf5179445ab0e7ba3521713a8574eb4fe718f
drosrod597 <drosrod597@g.educaand.es>
2023-12-17 18:45:10 +0100

    DataBase.class

--- src/Controller/DataBase.java
+++ src/Controller/DataBase.java
@@ -3,30 +3,30 @@ package Controller;
 import java.sql.*;
 
 public class DataBase {
-    String url = "jdbc:mysql://localhost:3307";
-    String user = "root";
-    String password = "mi-contrasena";
-    Statement stmt;
-    Connection conn;
-    ResultSet rs;
+    static String url = "jdbc:mysql://localhost:3307";
+    static String user = "root";
+    static String password = "mi-contrasena";
+    static Statement stmt;
+    static Connection conn;
     public DataBase() throws SQLException {
         createConnection();
         createStatement();
     }
-    public void createConnection() throws SQLException {
+    
+    public static void createConnection() throws SQLException {
         conn = null;
         conn = DriverManager.getConnection(url, user, password);
     }
 
-    public void createStatement() throws SQLException {
+    public static void createStatement() throws SQLException {
         stmt = conn.createStatement();
     }
 
-    public Statement obtenerStatement() throws SQLException {
+    public static Statement obtenerStatement() throws SQLException {
         return stmt;
     }
 
-    public void inicializar() throws SQLException {
+    public static void inicializar() throws SQLException {
         stmt.executeUpdate("DROP SCHEMA IF EXISTS Tienda");
         stmt.executeUpdate("CREATE SCHEMA IF NOT EXISTS Tienda");
         stmt.executeUpdate("USE Tienda");
@@ -37,7 +37,7 @@ public class DataBase {
         stmt.execute("INSERT INTO Cliente (user, password) VALUES ('Usuario', 'Contrasenia');");
     }
 
-    public void cerrar() {
+    public static void cerrar() {
         try {
             stmt.close();
         } catch (SQLException e) {
