918db19b0ad77aad614c9e24608427ecd1f812b9
drosrod597 <drosrod597@g.educaand.es>
2023-12-17 19:24:00 +0100

    Visualizar

---
+++ src/View/Visualizar.java
@@ -0,0 +1,15 @@
+package View;
+
+import javax.swing.*;
+
+public class Visualizar {
+    private JButton btn_anterior;
+    private JButton btn_siguiente;
+    private JButton btn_modificar;
+    private JButton btn_salir;
+    private JLabel lbl_id;
+    private JLabel lbl_nom;
+    private JLabel lbl_precio;
+    private JPanel jp_fecha;
+    private JLabel lbl_tit;
+}
