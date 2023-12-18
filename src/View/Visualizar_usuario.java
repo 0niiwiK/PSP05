0abe8ae2f48a845cd338a2c01f26f7aa95215ef2
drosrod597 <drosrod597@g.educaand.es>
2023-12-18 01:06:10 +0100

    visualizar usuario

--- src/View/Visualizar_usuario.java
+++ src/View/Visualizar_usuario.java
@@ -5,7 +5,7 @@ import Model.Cliente;
 import javax.swing.*;
 import java.awt.*;
 
-public class Visualizar_usuario {
+public class Visualizar_usuario extends JDialog {
     private JButton btn_cambiaPass;
     private JTextField tf_total;
     private JTextField tf_nombre;
@@ -16,18 +16,36 @@ public class Visualizar_usuario {
     private ImageIcon imagen;
     private Cliente cliente;
 
-    public Visualizar_usuario() {
+    public Visualizar_usuario(JFrame parent) {
+        super(parent, "Visualizar Usuario",true);
         tf_id.setEditable(false);
         tf_nombre.setEditable(false);
         tf_total.setEditable(false);
+        cliente = Init.cliente;
+        setContentPane(jpanel1);
+        tf_id.setText(String.valueOf(cliente.getId()));
+        tf_nombre.setText(cliente.getUser());
+        tf_total.setText(String.valueOf(cliente.getGastoTotal()));
         
-        imagen = new ImageIcon("src/Images/1001.jpg");
-        JLabel imageLabel = new JLabel(imagen);
-        jp_image.setLayout(new BorderLayout());
-        jp_image.add(imageLabel, BorderLayout.CENTER);
+        if (!cliente.getImagen().isEmpty()) {
+            imagen = new ImageIcon(cliente.getImagen());
+            JLabel imageLabel = new JLabel(imagen);
+            jp_image.setLayout(new BorderLayout());
+            jp_image.add(imageLabel, BorderLayout.CENTER);
+        }else {
+            imagen = new ImageIcon("src/Images/icon.png");
+            JLabel imageLabel = new JLabel(imagen);
+            jp_image.setLayout(new BorderLayout());
+            jp_image.add(imageLabel, BorderLayout.CENTER);
+        }
+        
         
         btn_cambiaPass.addActionListener(e -> {
             
         });
+    }
+    
+    public void cambiaPass() {
+        
     }
 }
